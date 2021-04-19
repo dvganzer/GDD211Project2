b430913dc4e4f9ae180cf48bbf84b5e4920de179
Derek V Ganzer <63074878+dvganzer@users.noreply.github.com>
2021-04-19 02:48:58 -0400

    Character Movement and Animation

---
+++ Project2GDD211/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Walkspeed = 5f;
+    public float Runspeed = 8f;
+    public static float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 move;
+    public Animator animator;
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Horizontal", move.x);
+       
+        animator.SetFloat("Speed", move.sqrMagnitude);
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Shoot", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Shoot", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+   
+    }
+
+}
